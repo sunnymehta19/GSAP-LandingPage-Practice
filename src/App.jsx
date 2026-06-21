f901891e002e303f85eb390b09607fdbab6ddef5
sunnymehta19 <whosunnymehta@gmail.com>
2026-06-22 00:17:54 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="section1">
         <nav className='flex justify-between px-10 py-6'>
           <div className="part1 flex items-center gap-1">
-            <RiShining2Fill className='rotate-45'/>
+            <RiShining2Fill className='rotate-45' />
             <h2 className='font-bold text-xl'>WizardZ</h2>
           </div>
           <div className="part2 flex gap-8 capitalize items-center">
@@ -22,8 +22,19 @@ function App() {
           </div>
         </nav>
 
-        <div className="hero">
-          
+        <div className="hero flex px-10 py-5">
+          <div className="left h-[70vh] w-1/2">
+            <div className="top h-[35vh]">
+              <h1 className="text-[48px] w-[70%] leading-15 font-neuMachina pl-5 pt-4">Navigating the digital landscape for success</h1>
+            </div>
+            <div className="bottom">
+              <div className="paragraph w-[85%] pl-5">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC. social media marketing and content creation. Helping brands increase visibility and reach the right audience.</div>
+              <button className='border px-6 rounded-lg border-2 py-2 ml-5 cursor-pointer text-[19px] bg-black text-white border-none my-4 '>Book a consultation</button>
+            </div>
+          </div>
+          <div className="right  h-[70vh] w-1/2">
+             <img src="/Hero.svg" alt="Hero" className='w-full h-full object-contain'/>
+          </div>
         </div>
       </div>
     </>
